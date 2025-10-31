eee75e3c88b8b7ad674e4097c36ce43afa6e19b5
chayansrk64 <chayansrk64@gmail.com>
2025-10-31 10:34:46 +0600

    get, post, patch and delete api created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
  
 // middleware
@@ -42,10 +42,42 @@ async function run () {
         const database = client.db("smart_db")
         const productsCollection = database.collection("products")
 
+        app.get("/products", async(req, res) => {
+            const cursor = productsCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get("/products/:id", async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await productsCollection.findOne(query)
+            res.send(result)
+        })
 
         app.post("/products", async(req, res) => {
             const newProduct = req.body;
             const result = await productsCollection.insertOne(newProduct)
+            res.send(result)
+        })
+
+        app.patch("/products/:id", async(req, res) => {
+            const id = req.params.id;
+            const updatedProduct = req.body;
+            const query = {_id: new ObjectId(id)}
+            const update = {
+              $set: {
+                  ...updatedProduct
+              }
+            }
+            const result = await productsCollection.updateOne(query, update)
+            res.send(result)
+        })
+
+        app.delete("/products/:id", async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await productsCollection.deleteOne(query)
             res.send(result)
         })
 
